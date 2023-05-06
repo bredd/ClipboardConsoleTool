ecbd73652739dd2d84d5fb0c886b975b9b4ce81c
Brandt Redd <brandt@redd.org>
2023-05-06 08:13:27 -0600

    support html

--- Program.cs
+++ Program.cs
@@ -9,28 +9,57 @@ using System.IO;
 
 namespace ClipboardTool
 {
+
     class Program
     {
+        const string cTypeSvg = "image/svg+xml";
+
         [STAThread]
         static void Main(string[] args)
         {
             try
             {
+                bool hasSvg = false;
+                bool hasHtml = false;
+
                 var dto = Clipboard.GetDataObject();
                 foreach (var format in dto.GetFormats())
                 {
                     Console.WriteLine(format);
+                    if (format.Equals(cTypeSvg, StringComparison.OrdinalIgnoreCase))
+                        hasSvg = true;
+                    if (format.Equals(DataFormats.Html))
+                        hasHtml = true;
                 }
                 Console.WriteLine();
 
-                var stream = dto.GetData("image/svg+xml") as Stream;
-                if (stream != null)
+                if (hasSvg)
                 {
-                    using (var dst = new FileStream(@"C:\users\brand\downloads\clipboard.svg", FileMode.Create, FileAccess.Write, FileShare.None))
+                    var stream = dto.GetData(cTypeSvg) as Stream;
+                    if (stream != null)
                     {
-                        stream.CopyTo(dst);
+                        using (var dst = new FileStream(@"C:\users\brand\downloads\clipboard.svg", FileMode.Create, FileAccess.Write, FileShare.None))
+                        {
+                            stream.CopyTo(dst);
+                        }
+                        Console.WriteLine("Copied SVG clipboard to downloads\\clipboard.svg");
                     }
-                    Console.WriteLine("Copied SVG clipboard to downloads\\clipboard.svg");
+                }
+
+                if (hasHtml)
+                {
+                    String str = dto.GetData(DataFormats.Html, true) as String;
+                    if (str != null)
+                    {
+                        int start = str.IndexOf("<!--StartFragment");
+                        start = str.IndexOf("-->", start + 17) + 3;
+                        int end = str.IndexOf("<!--EndFragment");
+                        using (var dst = new StreamWriter(@"C:\users\brand\downloads\clipboard.html", false, new UTF8Encoding(false)))
+                        {
+                            dst.Write(str.Substring(start, end-start));
+                        }
+                        Console.WriteLine("Copied HTML clipboard to downloads\\clipboard.html");
+                    }
                 }
             }
             catch (Exception err)
